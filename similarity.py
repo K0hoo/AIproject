15b478ca764dfca40adbc1c886bf9bde09f9c537
K0hoo <younghookwon@gm.gist.ac.kr>
2023-05-25 22:14:05 +0900

    similarity outline

---
+++ similarity.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+import csv
+
+
+def get_ingredient_similarity(menus:np.array, s_idx:int):
+    pass
+
+
+def get_menu_similarity(menus:np.array, s_idx:int):
+    pass
+
+
+def get_recipe_similarity(menus:np.array, s_idx:int):
+    pass
